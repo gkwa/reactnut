ac24814a675a53b8af6137a95f190a7030d1136d
Taylor Monacelli <taylormonacelli@gmail.com>
2023-05-08 11:31:11 -0700

    Bump version: 0.0.2 → 0.0.3

--- cmd/version.go
+++ cmd/version.go
@@ -20,7 +20,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("reactnut version 0.0.2")
+		fmt.Println("reactnut version 0.0.3")
 	},
 }
 
